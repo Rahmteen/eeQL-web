0bc37592aafb78e321e18edcd26bfb1dc7046316
Ramtin Khoee <ramtin.khoee@gmail.com>
2021-03-10 01:29:52 -0800

    react router/frontend boilerplate added

--- README.md
+++
@@ -1,2 +0,0 @@
-# reGal
-NFT upload abilities
--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-let w3Connect = document.getElementById('mm-button')
-
-w3Connect.addEventListener('click', () => {
-    ethereum.request({ method: 'eth_requestAccounts' });
-})
-
-})
